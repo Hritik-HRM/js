2e04421a04f3cbc527923e52915f1085644aa521
Hritik-HRM <121506757+Hritik-HRM@users.noreply.github.com>
2022-12-27 17:06:35 +0000

    datatype

---
+++ 01_Basic/1_DataVari.js
@@ -0,0 +1,17 @@
+"use strict" // use all js code as newer version
+
+const accountId = 2342
+let accoundEmail = "24245" // use it most of the time
+var accountPassword = "21212122121" // do not use it, because of issue in block scope and functional scope
+//accountCity = "Jaipur" // avoid it also// if use strict is there give error
+
+// accountId = 1 // gives TypeError: Assignment to constant variable
+
+
+let name = "Hritik"
+let age = 20
+let isLoggedIn = false
+let state
+
+console.table([typeof name, typeof age, typeof isLoggedIn, typeof state])
+console.log(typeof null) // object
